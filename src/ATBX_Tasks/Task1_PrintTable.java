2aa765f6c700f2b9a541edf6823421c89f10851a
Pooja <nskpoojagaydhani@gmail.com>
2025-05-15 20:06:19 +0530

    11th MAY 2025 task

---
+++ src/ATBX_Tasks/Task1_PrintTable.java
@@ -0,0 +1,18 @@
+package ATBX_Tasks;
+
+public class Task1_PrintTable {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Table of 5 using printf:");
+        System.out.printf("5 * 1 =%d%n", a);
+        System.out.printf("5 * 2 =%d%n", a * 2);
+        System.out.printf("5 * 3 =%d%n", a * 3);
+        System.out.printf("5 * 4 =%d%n", a * 4);
+        System.out.printf("5 * 5 =%d%n", a * 5);
+        System.out.printf("5 * 6 =%d%n", a * 6);
+        System.out.printf("5 * 7 =%d%n", a * 7);
+        System.out.printf("5 * 8 =%d%n", a * 8);
+        System.out.printf("5 * 9 =%d%n", a * 9);
+        System.out.printf("5 * 10 =%d%n", a * 10);
+    }
+}
