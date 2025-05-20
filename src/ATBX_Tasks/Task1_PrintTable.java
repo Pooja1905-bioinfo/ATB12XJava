0f74f21f472ba7ce5a20f5f91f71e74e3c4d3329
poojagaydhani <85831817+Pooja1905-bioinfo@users.noreply.github.com>
2025-05-20 18:37:10 +0530

    Update Task1_PrintTable.java

--- src/ATBX_Tasks/Task1_PrintTable.java
+++ src/ATBX_Tasks/Task1_PrintTable.java
@@ -4,15 +4,15 @@ public class Task1_PrintTable {
     public static void main(String[] args) {
         int a = 5;
         System.out.println("Table of 5 using printf:");
-        System.out.printf("5 * 1 =%d%n", a);
-        System.out.printf("5 * 2 =%d%n", a * 2);
-        System.out.printf("5 * 3 =%d%n", a * 3);
-        System.out.printf("5 * 4 =%d%n", a * 4);
-        System.out.printf("5 * 5 =%d%n", a * 5);
-        System.out.printf("5 * 6 =%d%n", a * 6);
-        System.out.printf("5 * 7 =%d%n", a * 7);
-        System.out.printf("5 * 8 =%d%n", a * 8);
-        System.out.printf("5 * 9 =%d%n", a * 9);
-        System.out.printf("5 * 10 =%d%n", a * 10);
+        System.out.printf("%d * 1 =%d%n", a);
+        System.out.printf("%d * 2 =%d%n", a * 2);
+        System.out.printf("%d * 3 =%d%n", a * 3);
+        System.out.printf("%d * 4 =%d%n", a * 4);
+        System.out.printf("%d * 5 =%d%n", a * 5);
+        System.out.printf("%d * 6 =%d%n", a * 6);
+        System.out.printf("%d * 7 =%d%n", a * 7);
+        System.out.printf("%d * 8 =%d%n", a * 8);
+        System.out.printf("%d * 9 =%d%n", a * 9);
+        System.out.printf("%d * 10 =%d%n", a * 10);
     }
 }
