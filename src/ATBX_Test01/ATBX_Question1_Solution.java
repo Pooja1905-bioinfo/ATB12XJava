0f56e43fbf0a69eb3393d4b25a227295156a7b9e
Pooja <nskpoojagaydhani@gmail.com>
2025-06-22 11:06:50 +0530

    Question1_Solution

---
+++ src/ATBX_Test01/ATBX_Question1_Solution.java
@@ -0,0 +1,14 @@
+package ATBX_Test01;
+
+public class ATBX_Question1_Solution {
+    public static void main(String[] args) {
+        int int1 = 25;
+        double d = 3.14;
+        boolean b = true;
+        char ch = 'A';
+        System.out.println(int1);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(ch);
+    }
+}
