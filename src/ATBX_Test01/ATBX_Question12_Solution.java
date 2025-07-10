83c3fdd8819e479135009edb3b22531d9393ac03
Pooja <nskpoojagaydhani@gmail.com>
2025-07-10 23:20:10 +0530

    test

--- src/ATBX_Test01/ATBX_Question12_Solution.java
+++ src/ATBX_Test01/ATBX_Question12_Solution.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 
 public class ATBX_Question12_Solution {
     public static void main(String[] args) {
-        System.out.println("Enter a positive number: ");
         Scanner scanner = new Scanner(System.in);
-
-        int fact = 7;
-
+        System.out.println("Please Enter a number: ");
         // n! = n*(n-1)*(n-2)*.......*2*1
         // input will be number n
         // 5! = 5*4*3*2*1
-        //
-        if ((fact == 1) || (fact == 0)) {
-            System.out.println("Factorial of given number is 1 ");
-        } else if (fact > 1) {
-            for (int i = fact - 1; i >= 1; i--) {
-                fact = fact * i;
-            }
-            System.out.println(fact);
-        } else {
-            System.out.println("Negative number factorial does not exist");
+        if (!scanner.hasNextInt()){
+            System.out.println("Invalid Input. Re-enter the number: ");
         }
+        int fact = 1;
+        int n = scanner.nextInt();
+        if(n <= 0) {
+            System.out.println("Factorial is always 1");
+        }
+        if(n > Integer.MAX_VALUE){
+            System.out.println("Cannot get Factorial as Out of Range int value");
+        }
+        int i = 1;
+        while ( i <= n ){
+            fact = fact * i;
+            i++;
+            //i = number, number- 1, number - 2
+        }
+        System.out.println(fact);
+
     }
 }
